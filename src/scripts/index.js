693b3fbfbbf6a6e194a069c238a57b53c358d3ee
Danil <danilsolom@mail.ru>
2021-01-09 16:27:55 +1000

    pinger

---
+++ src/scripts/index.js
@@ -0,0 +1,30 @@
+const https = require("http");
+
+const options = {
+	hostname: "dishesbook.herokuapp.com",
+	port: 80,
+	path: "/home",
+	method: "get",
+	headers: {
+		"Content-Type": "application/x-www-form-urlencoded",
+	},
+};
+setInterval(() => {
+	const req = https.request(options, (res) => {
+		console.log(`STATUS: ${res.statusCode}`);
+		console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+		res.setEncoding("utf8");
+		res.on("data", (chunk) => {
+			console.log(`BODY: ${chunk}`);
+		});
+		res.on("end", () => {
+			console.log(`${res.statusCode} - No more data in response.`);
+		});
+	});
+
+	req.on("error", (e) => {
+		console.log(`problem with request: ${e.message}`);
+	});
+
+	req.end();
+}, 1500000);
