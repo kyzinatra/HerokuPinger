95841a263586819c31d419ef2032bd370fe88884
Danil <danilsolom@mail.ru>
2021-01-09 17:07:46 +1000

    add int

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,20 +9,22 @@ const options = {
 		"Content-Type": "application/x-www-form-urlencoded",
 	},
 };
-const req = https.request(options, (res) => {
-	console.log(`STATUS: ${res.statusCode}`);
-	console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-	res.setEncoding("utf8");
-	res.on("data", (chunk) => {
-		console.log(`BODY: ${chunk}`);
+setInterval(() => {
+	const req = https.request(options, (res) => {
+		console.log(`STATUS: ${res.statusCode}`);
+		console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+		res.setEncoding("utf8");
+		res.on("data", (chunk) => {
+			console.log(`BODY: ${chunk}`);
+		});
+		res.on("end", () => {
+			console.log(`${res.statusCode} - No more data in response.`);
+		});
 	});
-	res.on("end", () => {
-		console.log(`${res.statusCode} - No more data in response.`);
+
+	req.on("error", (e) => {
+		console.log(`problem with request: ${e.message}`);
 	});
-});
 
-req.on("error", (e) => {
-	console.log(`problem with request: ${e.message}`);
-});
-
-req.end();
+	req.end();
+}, 1500000);
